a840fdf8a1bdf0832d7720135752c50ce4337c0e
Rayhan <rayhanwahyut27@gmail.com>
2026-01-07 17:44:18 +0700

    Disable Vercel Image Optimization to reduce usage

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    // Disable Vercel Image Optimization to reduce usage
+    // External images from Google Drive will be served directly
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
